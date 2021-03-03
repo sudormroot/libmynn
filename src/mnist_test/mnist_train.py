283567bfdbc977a537c8528ca935bc896c816ae3
sudormroot <sudormroot@163.com>
2021-03-03 16:12:43 +0000

    add mnist

--- src/mnist_test/mnist_train.py
+++ src/mnist_test/mnist_train.py
@@ -36,9 +36,9 @@ X_train, y_train, X_test, y_test = mnist_dataset_load(dataset_path)
 
 clf = MyMLPClassifier( n_input = N_IMAGE_PIXELS, 
                        n_output = N_LABELS, 
-                       hidden_sizes = (56, 28,), #define hidden layers
+                       hidden_sizes = (32, 16,), #define hidden layers
                        learning_rate = 0.05, 
-                       n_epochs = 1000, 
+                       n_epochs = 300, 
                        batch_size = 6,
                        alpha = 0.001,
                        #random_seed = 1,
